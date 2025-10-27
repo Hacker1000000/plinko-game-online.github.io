914267f559331bb03ca0863552f1c70402915523
Hacker1000000000 <95496750+Hacker1000000@users.noreply.github.com>
2025-10-27 14:13:55 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-
   kit: {
-    // Static site generation (SSG) is used: https://kit.svelte.dev/docs/adapter-static
-    adapter: adapter({
-      strict: false,
-    }),
+    adapter: adapter(),
   },
+  preprocess: vitePreprocess()
 };
 
 export default config;
